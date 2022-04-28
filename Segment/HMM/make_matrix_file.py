b8947a1082f635f0b44e9a9ae47998d6fd0ecb6e
Emmons <243633699@qq.com>
2022-04-28 19:48:30 +0800

    2022-04-28

--- Segment/HMM/make_matrix_file.py
+++ Segment/HMM/make_matrix_file.py
@@ -2,7 +2,7 @@ import sys,os
 sys.path.append('../')
 import pandas as pd
 import numpy as np
-from Segment.Utils.basic_utils import load_pkl,save_pkl
+from Segment.Utils.basic_utils import load_pkl,save_pkl,reverse_dict
 from Segment.DataProcess.data import WORD_COL,TAG_COL
 from tqdm import tqdm
 from pprint import pprint
@@ -16,9 +16,12 @@ class Make_HMM_Matrx():
         self.WordsAsTagDict = {}
         self.WordsAsTagDictProb = {}
         self.tag_dict = {}
+        self.label2tag = {}
 
         self.tag_sum = 0
         self.TagsDistribution = None
+
+        #这里默认是独立分词S，不然大概率会是B。分词任务中，不在字典的字就统一成S好了
         self.most_tag_name = None
 
         self.matrix_flag = False
@@ -58,7 +61,9 @@ class Make_HMM_Matrx():
         sum = np.sum(self.TagsDistribution, axis=1)
         self.TagsDistribution = (self.TagsDistribution.T / sum).T
 
-        pprint(self.TagsDistribution)
+        #替换一个极小值，让vertebi的时候求log or 求平方的时候不会报错
+        self.TagsDistribution[np.where(self.TagsDistribution==0.0)] = 1e-8
+        # pprint(self.TagsDistribution)
 
     def _get_dict_count(self):
         for index, item in tqdm(self.data.iterrows()):
@@ -88,29 +93,61 @@ class Make_HMM_Matrx():
                 else:
                     self.tag_dict[tag]=0
 
+
         self.tag_sum = len(self.tag_dict.keys())
-        most_tag_count = np.max(list(self.tag_dict.values()))
-        for item in self.tag_dict.keys():
-            if self.tag_dict[item] == most_tag_count:
-                self.most_tag_name = item
+
         for index,item in enumerate(self.tag_dict.keys()):
             self.tag_dict[item] = index
 
-        pprint(self.tag_dict)
+        self.label2tag = reverse_dict(self.tag_dict)
+        #分词任务中就不求最频繁了默认为S 即未登录字设计未单个词
+        self.most_tag_name = 'S'
+        # most_tag_count = np.max(list(self.tag_dict.values()))
+        # for item in self.tag_dict.keys():
+        #     if self.tag_dict[item] == most_tag_count:
+        #         self.most_tag_name = item
+
+
+        # pprint(self.tag_dict)
+        # pprint(self.label2tag)
         # print(self.most_tag_name,self.tag_sum)
 
 
 
     def load_from_path(self,model_path):
-        pass
+        load_dict = load_pkl(model_path)
+        self.WordsAsTagDict = load_dict['WordsAsTagDict']
+        self.WordsAsTagDictProb = load_dict['WordsAsTagDictProb']
+        self.tag_dict = load_dict['tag_dict']
+        self.label2tag = load_dict['label2tag']
+        self.tag_sum = load_dict['tag_sum']
+        self.TagsDistribution = load_dict['TagsDistribution']
+        self.most_tag_name = load_dict['most_tag_name']
+        self._verify_all_matrix()
+        if self.matrix_flag:
+            print('the hmm matrixs have been loaded successfully')
+        else:
+            warnings.warn('the hmm matrixs have some problems')
 
-    def save_matrix(self,save_path):
-        pass
+    def save_matrix(self,save_path,path_head):
+        save_dict = {
+            'WordsAsTagDict':self.WordsAsTagDict,
+            'WordsAsTagDictProb':self.WordsAsTagDictProb,
+            'tag_dict':self.tag_dict,
+            'label2tag':self.label2tag,
+            'tag_sum':self.tag_sum,
+            'TagsDistribution':self.TagsDistribution,
+            'most_tag_name':self.most_tag_name,
+        }
+        save_pkl(os.path.join(save_path,'{}_hmm.pkl'.format(path_head)),save_dict)
 
 if __name__ == '__main__':
     # print([0]*4)
     h = Make_HMM_Matrx()
     file_name = '../datas/ProcessData/msr_train.csv'
     h.make_with_train_file(file_name)
+    h.save_matrix(save_path='./matrixs',path_head='msr')
+
+    # h.load_from_path(model_path='./matrixs/msr_hmm.pkl')
     pass
 
--- Segment/HMM/make_matrixs_main.py
+++ Segment/HMM/make_matrixs_main.py
@@ -1,21 +1,91 @@
-import sys
+import sys,os,math
 sys.path.append('../')
 
+import numpy as np
 from Segment.HMM.make_matrix_file import Make_HMM_Matrx
 
+def segment(sent,tag):
+    res = ''
+    for i in range(len(sent)):
+        if tag[i] == 'S' or tag[i] == 'E':
+            res+=sent[i]+" "
+        else:
+            res+=sent[i]
+    return res
 
 class Vertebi():
-    def __init__(self,mode='train',**kwargs):
-        hmm_matrix = Make_HMM_Matrx()
+    def __init__(self,mode='train',model_path=None,data_path = None):
+        self.mode= mode
+        self.model_path = model_path
+        self.data_path = data_path
+        self.hmm = Make_HMM_Matrx()
+        self._init_hmm_matrix()
 
-    def _forward(self):
-        pass
 
-    def _get_max_pre(self):
-        pass
+    def _init_hmm_matrix(self):
+        if self.mode=='train' and not self.data_path==None:
+            self.hmm.make_with_train_file(self.data_path)
+        elif self.mode=='load' and not self.model_path==None:
+            self.hmm.load_from_path(self.model_path)
+        else:
+            assert "the mode is not vetify"
+        assert self.hmm.matrix_flag,"the hmm matrixs have not been corrtected established"
 
-    def _backword(self):
-        pass
+    def _getMaxPre(self,prob,Dic,ProDic,mode = 'sqrt'):
+        nextprob, maxpre = [], []
+        for i in range(len(Dic)):
+            problist = []
+            for j in range(len(prob)):
+                if mode == 'sqrt':
+                    problist.append(math.sqrt(prob[j] * Dic[i] * ProDic[j][i]))
+                elif mode== 'log':
+                    problist.append(math.log(prob[j] * Dic[i] * ProDic[j][i]))
+                else:
+                    problist.append(math.sqrt(prob[j] * Dic[i] * ProDic[j][i]))
+            nextprob.append(max(problist))
+            maxpre.append(problist.index(max(problist)))
 
-    def get_segment_tags(self):
-        pass
+        return nextprob, maxpre
+
+    def _wordpred(self,word, Dic):
+        if word in Dic:
+            return Dic[word]
+        else:
+            prob = [0.0]*self.hmm.tag_sum
+            prob[self.hmm.tag_dict[self.hmm.most_tag_name]]=1.0
+            return prob
+    def _forward(self,sequence):
+        prob = np.zeros((len(sequence), self.hmm.tag_sum))
+        maxpre = np.zeros((len(sequence) - 1, self.hmm.tag_sum))
+        prob[0] = self._wordpred(sequence[0], self.hmm.WordsAsTagDictProb)
+        for i in range(1, len(sequence)):
+            prob[i], maxpre[i - 1] = self._getMaxPre(prob[i - 1],
+                                   self._wordpred(sequence[i], self.hmm.WordsAsTagDictProb),
+                                   self.hmm.TagsDistribution)
+
+        lastmaxpro = prob[-1].tolist().index(max(prob[-1]))
+        return lastmaxpro, maxpre
+
+    def _backword(self,lastmaxpro,maxpre):
+        tag = []
+        tag.append(self.hmm.label2tag[lastmaxpro])
+        for i in range(len(maxpre) - 1, -1, -1):
+            lastmaxpro = (int)(maxpre[i].tolist()[lastmaxpro])
+            tag.append(self.hmm.label2tag[lastmaxpro])
+        tag.reverse()
+        res = "".join(tag)
+        return res
+
+    def get_segment_tags(self,words):
+        lastmaxpro, maxpre = self._forward(words)
+        tag = self._backword(lastmaxpro, maxpre)
+        res = segment(sequence,tag)
+        return tag,res
+
+if __name__ == '__main__':
+    sequence = '“北京健康宝”是一个方便个人查询自身防疫相关健康状态的小程序，所有在京及进（返）京人员均可使用。通过百度APP、微信、支付宝，'
+    v = Vertebi(mode='load',model_path='./matrixs/msr_hmm.pkl')
+    tag,res = v.get_segment_tags(sequence)
+    print(sequence)
+    print(tag)
+    print(res)
